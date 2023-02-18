19e5d7b1624562ecb04f676a4c376bfaeb0236aa
Rohmat MAC <rohmat661@gmail.com>
2023-02-19 04:45:17 +0700

    feat: - add library react navigation @6 - setup navigation - create folder container , component, & navigation - create file HomeScreen

---
+++ src/navigation/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../container/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootNavigation;
